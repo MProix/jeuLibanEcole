62b6bce795a67a607a80046c2af5074a5f9d0db0
Mélanie Proix <jnmelanie@gmail.com>
2020-11-17 22:42:50 +0100

    mobil version

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
         <title></title>
 
         <script src="./static/js/jquery-3.5.0.min.js"></script>
-        <script src="./static/js/main.js"></script>
-        
+
+
+        <link rel="stylesheet" href="./static/css/main.css">
     
     </head>
     <body id="body">
@@ -18,76 +19,93 @@
         </header>
         <nav>
             <ul id="myTabs" class="nav nav-pills" role="tablist">
-                <li role="presentation" class="nav_item active"><a href="#rules" aria-controls="rules" role="tab" data-toggle="pill">Règles</a></li>
-                <li role="presentation" class="nav_item"><a href="#game_board" aria-controls="game_board" role="tab" data-toggle="pill">Plateau de jeu</a></li>
-                <li role="presentation" class="nav_item"><a href="#gift" aria-controls="gift" role="tab" data-toggle="pill">Don</a></li>
-                <li role="presentation" class="nav_item"><a href="#explanations" aria-controls="explanations" role="tab" data-toggle="pill">Explications des cases</a></li>
-                <li role="presentation" class="nav_item"><a href="#quizz" aria-controls="quizz" role="tab" data-toggle="pill">Quizz</a></li>
+                <li id="activated_rules" role="presentation" class="nav_item active"><a href="#rules" aria-controls="rules" role="tab" data-toggle="pill">Règles</a></li>
+                <li id="activated_board_game" role="presentation" class="nav_item"><a href="#game_board" aria-controls="game_board" role="tab" data-toggle="pill">Plateau de jeu</a></li>
+                <li id="activated_gift" role="presentation" class="nav_item"><a href="#gift" aria-controls="gift" role="tab" data-toggle="pill">Don</a></li>
+                <li  id="activated_exp" role="presentation" class="nav_item"><a href="#explanations" aria-controls="explanations" role="tab" data-toggle="pill">Explications des cases</a></li>
+                <li id="activated_quizz" role="presentation" class="nav_item"><a href="#quizz" aria-controls="quizz" role="tab" data-toggle="pill">Quizz</a></li>
             </ul>
         </nav>
         <br><br>
         <div id="main_content" class="tab-content">
             <div role="tabpanel" class="tab-pane active" id="rules">
-                <h1>Règles du jeu</h1>
-                <h2>But du jeu</h2>
-                <p>S’amuser le plus possible en famille en participant aux épreuves proposées dans chaque case et faire un don pour l’école Notre Dame de la Paix du Liban.</p>
-                <h2>Joueurs</h2>
-                <p>Ce jeu est prévu pour votre famille. Vous pouvez choisir un seul joueur ou un joueur par membre de la famille. Mais les épreuves sont à faire ensemble. A vous de choisir si vous voulez refaire l’épreuve quand un autre membre tombe sur la case.</p>
-                <h2>Matériel</h2>
-                <p>Le plateau de jeu :  soit celui rapporté par votre enfant, soit celui imprimé par vos soins AJOUTER UN LIEN<br>
-                Un dé, un pion par joueur.<br>
-                Vous trouverez toutes les explications des épreuves ICI AJOUTER UN LIEN</p>
-                <h2>Déroulement</h2>
-                <p>Comme un jeu de l’oie avec sur le plateau, 3 sortes de cases :<br>
-                    Cases Liban : Quiz sur le Liban : utilisez la feuille Liban pour y répondre <br>
-                    Cases de couleur : des épreuves dont les recettes sont ICI <br>
-                    Cases spéciales  : </p>
+                <div class="title">
+                    <h1>Règles du jeu</h1>
+                </div>
+                <div class="part_content">
+                    <h2>But du jeu</h2>
+                    <p>S’amuser le plus possible en famille en participant aux épreuves proposées dans chaque case et faire un don pour l’école Notre Dame de la Paix du Liban.</p>
+                    <h2>Joueurs</h2>
+                    <p>Ce jeu est prévu pour votre famille. Vous pouvez choisir un seul joueur ou un joueur par membre de la famille, mais les épreuves sont à faire ensemble. À vous de choisir si vous voulez refaire l’épreuve quand un autre membre tombe sur la case.</p>
+                    <h2>Matériel</h2>
+                    <p>Le plateau de jeu : soit celui rapporté par votre enfant, soit celui <a href=#>imprimé par vos soins</a><br>
+                    Un dé, un pion par joueur.<br>
+                    <h2>Déroulement</h2>
+                    <p>Comme un jeu de l’oie avec trois sortes de cases :<br>
+                        Cases Liban : Quiz sur le Liban : utilisez la feuille Liban pour y répondre <br>
+                        Cases de couleur : des épreuves dont les explications sont <span><a href="#explanations" aria-controls="explanations" role="tab" data-toggle="pill" class="active_explanations">ICI</a></span> <br>
+                        Cases spéciales  : </p>
+                        <ul>
+                            <li>Case stop : prendre 15 secondes de silence</li>
+                            <li>Case cadeau : faire un compliment à un joueur</li>
+                            <li>Case lancer de dé (rejouer)</li>
+                        </ul>
+                        <p>Vous pouvez aussi utiliser ce jeu comme un calendrier de l’Avent et faire une case par jour : dans ce cas remplacez la case <strong>lancer de dé</strong> par un défi choisi en famille.</p>
+                    <h2>Don à Notre Dame du Liban...</h2>
+                    <p>...à ne pas oublier : cliquer <span><a href="#gift" aria-controls="gift" role="tab" data-toggle="pill" class="active_gift">ICI</a></span>.</p>
+                    <h2>Documents joints</h2>
                     <ul>
-                        <li>Case stop : prendre15 secondes de silence</li>
-                        <li>Case cadeau : faire un compliment à un joueur</li>
-                        <li>Case lancer de dé  (rejouer)</li>
+                        <li><a href="#">Règles du jeu  (PDF - ko) : Jouons pour le Liban</a></li>
+                        <li><a href="#">Plateau de jeu à imprimer si besoin</a></li>
+                        <li><a href="#">Explications des épreuves.pdf   Jouons pour le Liban</a></li>
                     </ul>
-                    <p>Vous pouvez aussi utiliser ce jeu comme un calendrier de l’avent et faire une case par jour : dans ce cas remplacez la case  lancer de dé par un défit choisi en famille.</p>
-                <h2>Don à Notre Dame du Liban...</h2>
-                <p>...à ne pas oublier : cliquer ICI.</p>
-                <h2>Documents joints</h2>
-                <ul>
-                    <li><a href="#">Règles du jeu  (PDF -  ko) : Jouons pour le Liban</a></li>
-                    <li><a href="#">Plateau de jeu à imprimer si besoin</a></li>
-                    <li><a href="#">Explications des épreuves.pdf   Jouons pour le Liban</a></li>
-                </ul>
+                </div>
             </div>
             <div role="tabpanel" class="tab-pane" id="game_board">
-                <h1>Plateau de jeu</h1>
-                <img src="./static/img/plateau.jpg">
-                <p>Vous pouvez aussi utiliser ce jeu comme un calendrier de l’avent et faire une case par jour : dans ce cas remplacez la consigne <strong>lancer de dé</strong> par un défi choisi en famille, ou prenez les cases dans l'ordre.</p>
-            </div>
+                <div class="title">
+                    <h1>Plateau de jeu</h1>
+                </div>
+                <div class="part_content">
+                    <img src="./static/img/plateau.jpg">
+                    <p>Vous pouvez aussi utiliser ce jeu comme un calendrier de l’Avent et faire une case par jour : dans ce cas remplacez la case <strong>lancer de dé</strong> par un défi choisi en famille.</p>            
+                </div>
             <div role="tabpanel" class="tab-pane" id="gift">
-                <h1>Notre don</h1>
-                <a href="#">(télécharger le rib)</a>
-                <p>Ce jeu nous l’avons préparé pour nous donnez la possibilité d’aider l’école Notre Dame de la Paix au Liban qui a bien souffert à cause de l’explosion.</p>
-                <p>Vous pouvez le faire :</p>
-                <ul>
-                    <li>Par chèque à l’ordre de l’école joie de vivre en écrivant au dos du chèque «DON POUR LE LIBAN».</li>
-                    <li>Par virement à l’école Joie de Vivre en précisant «DON POUR LE LIBAN» dans l’intitulé.</li>
-                    <li>Par espèces (par exemple faire une tirelire à la maison et la remplir jusqu’à Noël) à remettre sous enveloppe en précisant «DON POUR LE LIBAN»</li>
-                </ul>
-                <p>Merci pour votre générosité.</p>
+                <div class="title">
+                    <h1>Notre don</h1>
+                </div>
+                <div class="part_content">
+                    <a href="#">(télécharger le rib)</a>
+                    <p>Ce jeu nous l’avons préparé pour nous donner la possibilité d’aider l’école Notre Dame de la Paix au Liban qui a bien souffert à cause de l’<a href="#">explosion</a>.</p>
+                    <p>Vous pouvez le faire :</p>
+                    <ul>
+                        <li>Par chèque à l’ordre de l’école joie de vivre en écrivant au dos du chèque «DON POUR LE LIBAN».</li>
+                        <li>Par virement à l’école Joie de Vivre en précisant «DON POUR LE LIBAN» dans l’intitulé.</li>
+                        <li>Par espèces (par exemple faire une tirelire à la maison et la remplir jusqu’à Noël) à remettre sous enveloppe en précisant «DON POUR LE LIBAN»</li>
+                    </ul>
+                    <p>Merci pour votre générosité.</p>
 
-                <p>Les dons transitent par l'école pour ne verser qu'une fois les frais de virementvers le Liban qui sont élevés</p>
+                    <p>Les dons transitent par l'école pour ne verser qu'une fois les frais de virement vers le Liban qui sont élevés</p>
+                </div>
             </div>
             <div role="tabpanel" class="tab-pane" id="explanations">
-                <h1>Lumière sur les cases oranges</h1>
-                <p>Cliquer sur le numéro pour avoir l'explication !</p>
-                <h2>Recette de la case</h2>
+                <div class="title">
+                    <h1>Lumière sur les cases oranges</h1>
+                </div>
+                <div class="part_content">
+                    <p>Cliquer sur le numéro pour avoir l'explication !</p>
+                    <h2>Explication de la case</h2>
+                </div>
             </div>
             <div role="tabpanel" class="tab-pane" id="quizz">
-                <h1>Quizz</h1>
-            
+                <div class="title">
+                    <h1>Quizz</h1>
+                </div>
+                <div class="part_content"></div>
             </div>
         </div>
         </div>
-        <link rel="stylesheet" href="./static/css/main.css">
+        <script src="./static/js/main.js"></script>
+        
 
     </body>
 </html>
--- static/js/main.js
+++ static/js/main.js
@@ -16,166 +16,196 @@ function vhToPx(value){
     return (clientH*value)/100;
 };
 
-console.log(document.getElementById("body"));
-
-/* ========================================================================
- * Bootstrap: tab.js v3.4.1
- * https://getbootstrap.com/docs/3.4/javascript/#tabs
- * ========================================================================
- * Copyright 2011-2019 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
-
-+function ($) {
-    'use strict';
-  
-    // TAB CLASS DEFINITION
-    // ====================
-  
-    var Tab = function (element) {
-      // jscs:disable requireDollarBeforejQueryAssignment
-      this.element = $(element)
-      // jscs:enable requireDollarBeforejQueryAssignment
-    }
-  
-    Tab.VERSION = '3.4.1'
-  
-    Tab.TRANSITION_DURATION = 150
-  
-    Tab.prototype.show = function () {
-      var $this    = this.element
-      var $ul      = $this.closest('ul:not(.dropdown-menu)')
-      var selector = $this.data('target')
-  
-      if (!selector) {
-        selector = $this.attr('href')
-        selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+if (window.matchMedia("(min-width: 900px)").matches) {
+  $(".active_explanations").click(function(){
+    for (var elt of document.getElementById("myTabs").children){
+      for (var item of elt.classList){
+        if (item == "active"){
+          $(elt).removeClass("active");
+          $("#activated_exp").addClass("active");
+        }
       }
+    };
+  });
   
-      if ($this.parent('li').hasClass('active')) return
+  $(".active_gift").click(function(){
+    for (var elt of document.getElementById("myTabs").children){
+      for (var item of elt.classList){
+        if (item == "active"){
+          $(elt).removeClass("active");
+          $("#activated_gift").addClass("active");
+        }
+      }
+    };
+  });
   
-      var $previous = $ul.find('.active:last a')
-      var hideEvent = $.Event('hide.bs.tab', {
-        relatedTarget: $this[0]
-      })
-      var showEvent = $.Event('show.bs.tab', {
-        relatedTarget: $previous[0]
-      })
+  /* ========================================================================
+   * Bootstrap: tab.js v3.4.1
+   * https://getbootstrap.com/docs/3.4/javascript/#tabs
+   * ========================================================================
+   * Copyright 2011-2019 Twitter, Inc.
+   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+   * ======================================================================== */
   
-      $previous.trigger(hideEvent)
-      $this.trigger(showEvent)
   
-      if (showEvent.isDefaultPrevented() || hideEvent.isDefaultPrevented()) return
-  
-      var $target = $(document).find(selector)
-  
-      this.activate($this.closest('li'), $ul)
-      this.activate($target, $target.parent(), function () {
-        $previous.trigger({
-          type: 'hidden.bs.tab',
+  +function ($) {
+      'use strict';
+    
+      // TAB CLASS DEFINITION
+      // ====================
+    
+      var Tab = function (element) {
+        // jscs:disable requireDollarBeforejQueryAssignment
+        this.element = $(element)
+        // jscs:enable requireDollarBeforejQueryAssignment
+      }
+    
+      Tab.VERSION = '3.4.1'
+    
+      Tab.TRANSITION_DURATION = 150
+    
+      Tab.prototype.show = function () {
+        var $this    = this.element
+        var $ul      = $this.closest('ul:not(.dropdown-menu)')
+        var selector = $this.data('target')
+    
+        if (!selector) {
+          selector = $this.attr('href')
+          selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+        }
+    
+        if ($this.parent('li').hasClass('active')) return
+    
+        var $previous = $ul.find('.active:last a')
+        var hideEvent = $.Event('hide.bs.tab', {
           relatedTarget: $this[0]
         })
-        $this.trigger({
-          type: 'shown.bs.tab',
+        var showEvent = $.Event('show.bs.tab', {
           relatedTarget: $previous[0]
         })
-      })
-    }
-  
-    Tab.prototype.activate = function (element, container, callback) {
-      var $active    = container.find('> .active')
-      var transition = callback
-        && $.support.transition
-        && ($active.length && $active.hasClass('fade') || !!container.find('> .fade').length)
-  
-      function next() {
-        $active
-          .removeClass('active')
-          .find('> .dropdown-menu > .active')
-          .removeClass('active')
-          .end()
-          .find('[data-toggle="tab"]')
-          .attr('aria-expanded', false)
-  
-        element
-          .addClass('active')
-          .find('[data-toggle="tab"]')
-          .attr('aria-expanded', true)
-  
-        if (transition) {
-          element[0].offsetWidth // reflow for transition
-          element.addClass('in')
-        } else {
-          element.removeClass('fade')
-        }
-  
-        if (element.parent('.dropdown-menu').length) {
-          element
-            .closest('li.dropdown')
-            .addClass('active')
+    
+        $previous.trigger(hideEvent)
+        $this.trigger(showEvent)
+    
+        if (showEvent.isDefaultPrevented() || hideEvent.isDefaultPrevented()) return
+    
+        var $target = $(document).find(selector)
+    
+        this.activate($this.closest('li'), $ul)
+        this.activate($target, $target.parent(), function () {
+          $previous.trigger({
+            type: 'hidden.bs.tab',
+            relatedTarget: $this[0]
+          })
+          $this.trigger({
+            type: 'shown.bs.tab',
+            relatedTarget: $previous[0]
+          })
+        })
+      }
+    
+      Tab.prototype.activate = function (element, container, callback) {
+        var $active    = container.find('> .active')
+        var transition = callback
+          && $.support.transition
+          && ($active.length && $active.hasClass('fade') || !!container.find('> .fade').length)
+    
+        function next() {
+          $active
+            .removeClass('active')
+            .find('> .dropdown-menu > .active')
+            .removeClass('active')
             .end()
             .find('[data-toggle="tab"]')
+            .attr('aria-expanded', false)
+    
+          element
+            .addClass('active')
+            .find('[data-toggle="tab"]')
             .attr('aria-expanded', true)
+    
+          if (transition) {
+            element[0].offsetWidth // reflow for transition
+            element.addClass('in')
+          } else {
+            element.removeClass('fade')
+          }
+    
+          if (element.parent('.dropdown-menu').length) {
+            element
+              .closest('li.dropdown')
+              .addClass('active')
+              .end()
+              .find('[data-toggle="tab"]')
+              .attr('aria-expanded', true)
+          }
+    
+          callback && callback()
         }
-  
-        callback && callback()
+    
+        $active.length && transition ?
+          $active
+            .one('bsTransitionEnd', next)
+            .emulateTransitionEnd(Tab.TRANSITION_DURATION) :
+          next()
+    
+        $active.removeClass('in')
       }
+    
+    
+      // TAB PLUGIN DEFINITION
+      // =====================
+    
+      function Plugin(option) {
+        return this.each(function () {
+          var $this = $(this)
+          var data  = $this.data('bs.tab')
+    
+          if (!data) $this.data('bs.tab', (data = new Tab(this)))
+          if (typeof option == 'string') data[option]()
+        })
+      }
+    
+      var old = $.fn.tab
+    
+      $.fn.tab             = Plugin
+      $.fn.tab.Constructor = Tab
+    
+    
+      // TAB NO CONFLICT
+      // ===============
+    
+      $.fn.tab.noConflict = function () {
+        $.fn.tab = old
+        return this
+      }
+    
+    
+      // TAB DATA-API
+      // ============
+    
+      var clickHandler = function (e) {
+        e.preventDefault()
+        Plugin.call($(this), 'show')
+      }
+    
+      $(document)
+        .on('click.bs.tab.data-api', '[data-toggle="tab"]', clickHandler)
+        .on('click.bs.tab.data-api', '[data-toggle="pill"]', clickHandler)
+    
+    }(jQuery);
   
-      $active.length && transition ?
-        $active
-          .one('bsTransitionEnd', next)
-          .emulateTransitionEnd(Tab.TRANSITION_DURATION) :
-        next()
-  
-      $active.removeClass('in')
-    }
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      e.target // newly activated tab
+      e.relatedTarget // previous active tab
+  });
   
   
-    // TAB PLUGIN DEFINITION
-    // =====================
-  
-    function Plugin(option) {
-      return this.each(function () {
-        var $this = $(this)
-        var data  = $this.data('bs.tab')
-  
-        if (!data) $this.data('bs.tab', (data = new Tab(this)))
-        if (typeof option == 'string') data[option]()
-      })
-    }
-  
-    var old = $.fn.tab
-  
-    $.fn.tab             = Plugin
-    $.fn.tab.Constructor = Tab
-  
-  
-    // TAB NO CONFLICT
-    // ===============
-  
-    $.fn.tab.noConflict = function () {
-      $.fn.tab = old
-      return this
-    }
-  
-  
-    // TAB DATA-API
-    // ============
-  
-    var clickHandler = function (e) {
-      e.preventDefault()
-      Plugin.call($(this), 'show')
-    }
-  
-    $(document)
-      .on('click.bs.tab.data-api', '[data-toggle="tab"]', clickHandler)
-      .on('click.bs.tab.data-api', '[data-toggle="pill"]', clickHandler)
-  
-  }(jQuery);
-
-$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    e.target // newly activated tab
-    e.relatedTarget // previous active tab
-});
+} else {
+    $(".title").click(function(){
+      $($(this)[0].nextElementSibling).toggle();
+      // $(this).nextElementSibling.toggle();  
+    })
+    $("#game_board>div>img").attr('src','./static/img/plateau_2.jpg');
+}
 
